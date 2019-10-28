d4391ea7aeb5a19cc77877ae3d282e6268e49695
vincent-tr <vincent@stoic.com>
2019-10-28 09:25:44 +0100

    builders + config

---
+++ bin/build.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const { buildWeb } = require('./lib/build-web');
+
+main();
+
+async function main() {
+
+  const baseDirectory = process.cwd();
+  const { mylifeBuild: config } = require(path.join(baseDirectory, 'package.json'));
+
+  if(!config) {
+    return;
+  }
+
+  if(config.web) {
+    const result = await buildWeb(baseDirectory);
+    if(!result) {
+      process.exit(-1);
+    }
+  }
+}
