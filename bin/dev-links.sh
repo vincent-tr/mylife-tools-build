cf91ed80ebf1aa73279552ac349cbee9d35d58d3
vincent-tr <vincent@stoic.com>
2019-11-26 09:33:23 +0100

    dev links

--- bin/dev-links.sh
+++ bin/dev-links.sh
@@ -4,3 +4,29 @@ base_dir="$PWD/.."
 current=$(basename $PWD)
 
 # TODO: check in dependencies if package present
+
+pushd $base_dir/mylife-tools-common
+npm link
+popd
+
+pushd $base_dir/mylife-tools-build
+npm link
+popd
+
+pushd $base_dir/mylife-tools-ui
+npm link
+npm link mylife-tools-common
+popd
+
+pushd $base_dir/mylife-tools-server
+npm link
+npm link mylife-tools-common
+npm link mylife-tools-build
+popd
+
+pushd $base_dir/$current
+npm link mylife-tools-common
+npm link mylife-tools-server
+npm link mylife-tools-ui
+npm link mylife-tools-build
+popd
